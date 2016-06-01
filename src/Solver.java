294387c6ff134a763e62bfb780ffb6f3fb55c40a
Bruno Vitorino <bruno.vitorino@gmail.com>
2016-06-01 22:14:39 +0200

    Initial commit

---
+++ src/Solver.java
@@ -0,0 +1,26 @@
+import java.util.Set;
+
+public class Solver {
+	private static final int I = 2;
+	private static final int K = 5;
+	private static final int NB_LIST_SIZE = 20;
+	
+	private int M;
+	
+	public Solver() {
+
+	}
+
+	public void solve() {
+		
+	}
+	
+	private void sweep(Set<Point> cities) {
+
+	}
+	
+	public static void main(String[] args) {
+		Solver solver = new Solver();
+		solver.solve();
+	}
+}
